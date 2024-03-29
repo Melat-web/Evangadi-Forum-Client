fa66df0173ff66a74918c4c451b8bc47fea75930
Melat-web <melatworkbekele27@gmail.com>
2024-03-29 22:27:42 +0300

    updated url

--- src/AxiosConfigration/axiosConfig.jsx
+++ src/AxiosConfigration/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 const axiosBase = axios.create({
-  // baseURL: "https://latest-evangadi-forum-server.vercel.app/api",
+  baseURL: "https://regal-biscuit-6e9130.netlify.app",
 
-  baseURL: "http://localhost:5500/api",
+  // baseURL: "http://localhost:5500/api",
 });
 
 export default axiosBase;
